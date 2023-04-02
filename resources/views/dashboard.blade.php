c5d167882f009adb748b80ff5762345bd2508d34
inahcauo01 <tarek.ouachani01@gmail.com>
2023-04-02 01:11:26 +0000

    working on feed page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,104 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+    <div class="py-3">
+
+    <main class="main">
+        {{-- left bar --}}
+        <aside class="sidebar-left">
+            <div class="trending">
+                <h2>Trending Topics</h2>
+                <ul>
+                    <li><a href="#">#socialmedia</a></li>
+                    <li><a href="#">#marketing</a></li>
+                    <li><a href="#">#technology</a></li>
+                    <li><a href="#">#business</a></li>
+                    <li><a href="#">#startup</a></li>
+                </ul>
+            </div>
+            <div class="links">
+                <h2>Useful Links</h2>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms of Service</a></li>
+                </ul>
+            </div>
+        </aside>
+
+        {{-- feed content (middle) --}}
+        <div class="feed">
+            <div class="post">
+                <div class="post-header">
+                    <img src="{{ asset('images/default.png') }}" alt="User Avatar">
+                    <div class="post-header-details">
+                        <h2>John Doe</h2>
+                        <p>2 hours ago</p>
+                    </div>
+                </div>
+                <div class="post-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique ante nulla, a laoreet risus vehicula a. Donec eu est at enim mollis dignissim.</p>
+                    <img src="{{ asset('images/abstract-landing2.jpg') }}" alt="Post Image">
+                </div>
+                <div class="post-actions">
+                    <button class="like-button">Like</button>
+                    <button class="comment-button">Comment</button>
+                    <button class="share-button">Share</button>
+                </div>
+                <div class="comments">
+                    <div class="comment">
+                        <img src="{{ asset('images/default.png') }}" alt="User Avatar">
+                        <div class="comment-details">
+                            <h3>Jane Smith</h3>
+                            <p>2 hours ago</p>
+                            <p>Great post, John!</p>
+                        </div>
+                    </div>
+                <div class="comment">
+                    <img src="{{ asset('images/default.png') }}" alt="User Avatar">
+                    <div class="comment-details">
+                        <h3>Bob Johnson</h3>
+                        <p>1 hour ago</p>
+                        <p>Thanks for sharing, John.</p>
+                    </div>
+                </div>
+                <form class="comment-form">
+                    <img src="{{ asset('images/default.png') }}" alt="User Avatar">
+                    <input type="text" placeholder="Add a comment...">
+                    <button type="submit">Post</button>
+                </form>
+                </div>
             </div>
         </div>
-    </div>
+
+        {{-- right bar --}}
+        <aside class="sidebar-right">
+            <div class="suggested-friends">
+                <h2>Suggested Friends</h2>
+                <ul>
+                    <li>
+                        <img src="{{ asset('images/default.png') }}" alt="User Avatar">
+                        <div class="friend-details">
+                            <h3>Jane Smith</h3>
+                            <button>Add Friend</button>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="{{ asset('images/default.png') }}" alt="User Avatar">
+                        <div class="friend-details">
+                            <h3>Bob Johnson</h3>
+                            <button>Add Friend</button>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="{{ asset('images/default.png') }}" alt="User Avatar">
+                        <div class="friend-details">
+                            <h3>Sarah Williams</h3>
+                            <button>Add Friend</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+    </main>
 </x-app-layout>
